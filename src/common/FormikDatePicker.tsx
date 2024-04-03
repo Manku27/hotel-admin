ffb8470ff9c7e58e090b9337d5d146ed975076d5
Mayank Jhunjhunwala <v-mayankj@microsoft.com>
2024-04-03 13:29:02 +0530

    add date picker

---
+++ src/common/FormikDatePicker.tsx
@@ -0,0 +1,30 @@
+import { ErrorMessage, Field } from 'formik';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers';
+import { FormHelperText } from '@mui/material';
+
+const FormikDatePicker = ({ name, ...otherProps }: any) => {
+  return (
+    <Field name={name}>
+      {({ field, form: { setFieldValue, errors } }) => (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            value={field.value ? field.value : null}
+            onChange={(value) => {
+              setFieldValue(field.name, value ? value : '');
+            }}
+            {...otherProps}
+          />
+          {errors ? (
+            <FormHelperText>
+              <ErrorMessage name={field.name} />
+            </FormHelperText>
+          ) : null}
+        </LocalizationProvider>
+      )}
+    </Field>
+  );
+};
+
+export default FormikDatePicker;
