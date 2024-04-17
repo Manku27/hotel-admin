f603e4a6e124e85daf3a4c0885c697153906760e
Mayank Jhunjhunwala <v-mayankj@microsoft.com>
2024-04-17 19:56:56 +0530

    add swr and react-toastify

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import theme from './theme.tsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <ToastContainer />
       <App />
     </ThemeProvider>
   </React.StrictMode>
